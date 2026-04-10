7e0e758f4a8b6d739271ef08a05ac5852f26b287
NamNguyen237 <138494161+NamNguyen237@users.noreply.github.com>
2026-04-10 09:03:05 +0700

    Create lopphanso.cpp

---
+++ Lap trinh huong doi tuong/buoi 6 - ham thiet lap/lopphanso.cpp
@@ -0,0 +1,146 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Phanso {
+    private:
+        int tu, mau;
+    public:
+        void Nhap();
+        void Hienthi(string str = "");
+        Phanso Tong(Phanso ps);
+        Phanso Hieu(Phanso ps);
+        Phanso Tich(Phanso ps);
+        Phanso Thuong(Phanso ps);
+        Phanso ToiGian();
+        Phanso &Toigian(); //ham tra ve tham chieu
+        void Toigian2();
+        //ham thiet lap
+        Phanso();
+        Phanso(int i, int m);
+        ~Phanso(); //ham huy thiet lap
+};
+Phanso::Phanso() {
+    cout << "\nHTL tai " << this;
+    tu = 0;
+    mau = 1;
+}
+Phanso::Phanso(int t, int m)
+{
+    cout << "\nHTL tai " << this;
+    if (m == 0) {
+        tu = t;
+        mau = 1;
+    }
+    else if ( m > 0 ){
+        tu = t;
+        mau = m;
+    }
+    else { // m < 0
+        tu = -t;
+        mau = -m;
+    }
+}
+Phanso::~Phanso()
+{
+    cout << "\nHHB tai " << this;
+}
+void Phanso::Nhap() {
+    int t, m;
+    do {
+        cout << "\nNhap tu so: ";
+        cin >> t;
+        cout << "\nNhap mau so != 0: ";
+        cin >> m;
+    } while (m == 0);
+    if ( m > 0 ){
+        tu = t;
+        mau = m;
+    }
+    else { // m < 0
+        tu = -t;
+        mau = -m;
+    } // muc dich: luon luon dua mau so ve duong
+}
+void Phanso::Hienthi(string str) {
+    cout << str;
+    if (mau == 1 || tu == 0) cout << tu;
+    else cout << tu << "/" << mau;
+}
+void Phanso::Toigian2() { //toi gian truc tiep
+    int ucln = gcd(tu,mau);
+    tu /= ucln;
+    mau /= ucln;
+}
+Phanso Phanso::ToiGian() { //tao ra phan so moi
+    int ucln = gcd(tu,mau);
+    tu /= ucln;
+    mau /= ucln;
+    return *this;
+}
+
+Phanso & Phanso::Toigian() { //tra ve tham chieu
+    int ucln = gcd(tu,mau);
+    tu /= ucln;
+    mau /= ucln;
+    return *this;
+}
+Phanso Phanso::Tong(Phanso ps) {
+    Phanso kq;
+    kq.tu = tu * ps.mau + ps.tu * mau;
+    kq.mau = mau * ps.mau;
+    //kq.Toigian2();//toi gian trong ham luon
+    return kq;
+}
+Phanso Phanso::Hieu(Phanso ps) {
+    Phanso kq;
+    kq.tu = tu * ps.mau - ps.tu * mau;
+    kq.mau = mau * ps.mau;
+    //kq.Toigian2();//toi gian trong ham luon
+    return kq;
+}
+Phanso Phanso::Tich(Phanso ps) {
+    Phanso kq;
+    kq.tu = tu * ps.tu;
+    kq.mau = mau * ps.mau;
+    //kq.Toigian2();//toi gian trong ham luon
+    return kq;
+}
+Phanso Phanso::Thuong(Phanso ps) {
+    Phanso kq;
+    kq.tu = tu * ps.mau;
+    kq.mau = mau * ps.tu;
+    //kq.Toigian2();//toi gian trong ham luon
+    return kq;
+}
+
+int main()
+{
+    Phanso ps1; //tu chay ham thiet lap khi khai bao doi tuong cua class
+    ps1.Hienthi("\nps1 = "); cout << endl;
+    ps1.Nhap();
+    ps1.Hienthi("\nps1 = "); cout << endl;
+    
+    Phanso ps2(3,4);
+    ps2.Hienthi("\nps2 = "); cout << endl;
+    ps2.Nhap();
+    ps2.Hienthi("\nps2 = "); cout << endl;
+
+    
+    Phanso tong = ps1.Tong(ps2);
+    tong.Hienthi("\nps1 + ps2 = ");
+    tong.ToiGian().Hienthi("\nps1 + ps2 toi gian = "); cout << endl;
+
+    Phanso hieu = ps1.Hieu(ps2);
+    hieu.Hienthi("\nps1 - ps2 = ");
+    hieu.ToiGian().Hienthi("\nps1 - ps2 toi gian = "); cout << endl;
+
+    Phanso tich = ps1.Tich(ps2);
+    tich.Hienthi("\nps1 * ps2 = ");
+    tich.ToiGian().Hienthi("\nps1 * ps2 toi gian = "); cout << endl;
+
+    Phanso thuong = ps1.Thuong(ps2);
+    thuong.Hienthi("\nps1 / ps2 = ");
+    thuong.ToiGian().Hienthi("\nps1 / ps2 toi gian = "); cout << endl;
+
+    return 0;
+}
